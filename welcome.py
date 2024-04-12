50e9c8167e9d3fec9a68ed1b1e5cc5583e77c311
Markkus-Toriland <163395778+Markkus-Toriland@users.noreply.github.com>
2024-04-12 09:08:38 +0300

    Feature a 2 (#3)
    * Add age validation
    
    * Ask user name

--- welcome.py
+++ welcome.py
@@ -1,4 +1,14 @@
 from validate_age import *
+from get_name import *
+
+print('='*35)
+print('Welcome to voting eligibility check')
+print('='*35)
+print('\n')
+
+user_name = get_name()
+print(f'Hello {user_name}!\n')
 
 age = int(input("Enter your age: "))
 validate_age(age)
+
