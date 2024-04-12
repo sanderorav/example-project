9d0b25623d43f0c609eb93d87286214bfed19c78
markkus <markkus.toriland@voco.ee>
2024-04-12 09:06:38 +0300

    Ask user name

--- welcome.py
+++ welcome.py
@@ -1,4 +1,9 @@
-from validate_age import *
+from get_name import *
 
-age = int(input("Enter your age: "))
-validate_age(age)
+print('='*35)
+print('Welcome to voting eligibility check')
+print('='*35)
+print('\n')
+
+user_name = get_name()
+print(f'Hello {user_name}!\n')
